4f31f3a59eb17b803db23df2527641e6f11c8cb8
User <lifesim11@gmail.com>
2024-01-22 18:35:27 +0100

    deploy fixes

---
+++ sanity/env.ts
@@ -0,0 +1,25 @@
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+export const apiVersion = process.env.SANITY_STUDIO_API_VERSION || "2023-12-03";
+
+export const dataset = assertValue(
+  process.env.SANITY_STUDIO_DATASET,
+  "Missing environment variable: SANITY_STUDIO_DATASET"
+);
+
+export const projectId = assertValue(
+  process.env.SANITY_STUDIO_PROJECT_ID,
+  "Missing environment variable: SANITY_STUDIO_SANITY_PROJECT_ID"
+);
+
+export const useCdn = false;
+
+function assertValue<T>(v: T | undefined, errorMessage: string): T {
+  if (v === undefined) {
+    throw new Error(errorMessage);
+  }
+
+  return v;
+}
